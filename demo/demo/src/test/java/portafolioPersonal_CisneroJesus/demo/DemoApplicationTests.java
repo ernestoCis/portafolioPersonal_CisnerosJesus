afbc08fdb6fbc4cafd8bf490c610c1c973ec0a88
ernestoCis <jesus.cisneros262722@potros.itson.edu.mx>
2026-09-04 15:09:43 -0700

    practica02

---
+++ demo/demo/src/test/java/portafolioPersonal_CisneroJesus/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package portafolioPersonal_CisneroJesus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
